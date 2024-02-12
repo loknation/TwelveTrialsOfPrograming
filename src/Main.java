959125385dde60f6c0e64c36f4073372f742aae4
loknation <pierce.noltemeyer@talawanda.org>
2024-02-12 14:07:42 -0500

    asdf

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class Main {
+    public static void Main(String[] args){
+        System.out.println("What trial do you want to do");
+        Scanner Scan = new Scanner(System.in);
+        int input = 0;
+        try {
+            input = Integer.parseInt(Scan.nextLine());
+        }
+        catch (Exception E){
+            System.out.println("Enter a Number");
+        }
+        if (input == 1){
+            System.out.println("Under Development");
+        }
+        else if (input == 2){
+            System.out.println("Under Development");
+        }
+        else if (input == 3){
+            System.out.println("Under Development");
+        }
+        else if (input == 4){
+            System.out.println("Under Development");
+        }
+        else if (input == 5){
+            System.out.println("Under Development");
+        }
+        else if (input == 6){
+            System.out.println("Under Development");
+        }
+        else if (input == 7){
+            System.out.println("Under Development");
+        }
+        else if (input == 8){
+            System.out.println("Under Development");
+        }
+        else if (input == 9){
+            System.out.println("Under Development");
+        }
+        else if (input == 10){
+            System.out.println("Under Development");
+        }
+        else if (input == 11){
+            System.out.println("Under Development");
+        }
+        else if (input == 12){
+            System.out.println("Under Development");
+        }
+
+    }
+}
---
+++ src/Trial3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Trial3 {
+    public static double ConvertTime( int Time){
+        System.out.println("hi");
+    }
+    public static void run(){
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("How far are you traveling in miles");
+
+    }
+}
