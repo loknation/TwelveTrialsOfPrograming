d99844d57a4041d9c510c0a925d532e81d716715
loknation <114083950+loknation@users.noreply.github.com>
2024-02-12 16:09:01 -0500

    Trial1.java

---
+++ src/Trial1.java
@@ -0,0 +1,14 @@
+public class Trial1 {
+    public void run(){
+        System.out.println("  /\\__/\\");
+        System.out.println(" /      \\");
+        System.out.println("(  O   O )");
+        System.out.println(" \\  V  / ");
+        System.out.println("  |   |  ");
+        System.out.println("  |___|  ");
+        System.out.println(" /  |  \\ ");
+        System.out.println("/   |   \\");
+        System.out.println("|___|___|");
+        System.out.println("I Tried :(");
+    }
+}
